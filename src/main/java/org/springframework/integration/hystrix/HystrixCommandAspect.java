b8f3caa9fdb20be29ae41192fd4d4c3c0f7e4e8c
maluyong <maluyong@tuniu.com>
2016-12-25 12:53:33 +0800

    loc-3 添加注解。

--- src/main/java/org/springframework/integration/hystrix/HystrixCommandAspect.java
+++ src/main/java/org/springframework/integration/hystrix/HystrixCommandAspect.java
@@ -29,7 +29,7 @@ public class HystrixCommandAspect {
 			return getHystrixCommand(joinPoint, cb).execute();
 		} catch (HystrixRuntimeException e) {
 
-			//将主线的异常根据注解的配置进行降级或者是其他的一些操作。
+			//将主线的异常根据注解的配置进行降级或者是其他的一些操作。  多说一句，将这个注解传递进来的时候，其实是传递了很多的信息量的。
 			return ExceptionUtil.handleException(e, joinPoint, cb);
 		}
 	}
